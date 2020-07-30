399d337d46124c7c923a143f5fee2894b3efaa8e
Matt Keane <mattpkeane@gmail.com>
2020-07-30 18:38:08 -0500

    moved functions into an object and created state

--- static/app.js
+++ static/app.js
@@ -1,40 +1,49 @@
-// function to create a new pixel div
+const board = {
 
-const createNewPixel = () => {
-	const newPixel = document.createElement("div")
-	newPixel.setAttribute("class", "pixel")
-	return newPixel
-}
+	// array to hold values representing pixels
+	pixels: [],
 
-// function to create a row of pixels
-const createNewRow = (length) => {
-	const newRow = document.createElement("div")
-	newRow.setAttribute("class", "row")
-	for (let i = 0; i < length; i++) {
-		newRow.appendChild(createNewPixel())
+	// event handlers
+
+	handleClick: function(e) {
+		if (e.target.className === "pixel") {
+			e.target.setAttribute("class", "white-pixel")
+		} else if (e.target.className === "white-pixel") {
+			e.target.setAttribute("class", "pixel")
+		} else {
+			console.log("Error: target's class name is invalid")
+		}
+		console.log(this.pixels)
+	},	
+
+	// method to create a new pixel in DOM and state
+	createNewPixel: function(row) {
+		this.pixels[row].push(0)
+		const newPixel = document.createElement("div")
+		newPixel.setAttribute("class", "pixel")
+		return newPixel
+	},
+
+	// method to create new row in DOM and state
+	createNewRow: function(row, length) {
+		this.pixels.push([])
+		const newRow = document.createElement("div")
+		newRow.setAttribute("class", "row")
+		for (let i = 0; i < length; i++) {
+			newRow.appendChild(this.createNewPixel(row))
+		}
+		return newRow
+	},
+
+	create: function(width, height) {
+		const newBoard = document.createElement("div")
+		newBoard.setAttribute("class", "board")
+		for (let i = 0; i < height; i++) {
+			newBoard.appendChild(this.createNewRow(i, width))
+		}
+		newBoard.addEventListener("click", e => this.handleClick(e))
+		return newBoard
 	}
-	return newRow
 }
 
-const handleClick = e => {
-	if (e.target.className === "pixel") {
-		e.target.setAttribute("class", "white-pixel")
-	} else if (e.target.className === "white-pixel") {
-		e.target.setAttribute("class", "pixel")
-	} else {
-		console.log("Error: target's class name is invalid")
-	}
-}
-
-// function to create the board of pixels
-const createBoard = (width, height) => {
-	const board = document.createElement("div")
-	board.setAttribute("class", "board")
-	for (let i = 0; i < height; i++) {
-		board.appendChild(createNewRow(width))
-	}
-	board.addEventListener("click", handleClick)
-	return board
-}
-
-document.querySelector("body").appendChild(createBoard(10, 10))
+document.querySelector("body").appendChild(board.create(10, 10))
