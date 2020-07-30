be29a3190e2bfbdab7e56cc9c771b6f69cdf3900
Matt Keane <mattpkeane@gmail.com>
2020-07-30 16:21:35 -0500

    Initialized app, test route works

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+DEBUG = True
+PORT = 8000
+
+app = Flask(__name__)
+
+@app.route("/")
+def test_route():
+	return "Hewwo World! UwU"
+
+if __name__ == "__main__":
+	app.run(debug=DEBUG, port=PORT)
