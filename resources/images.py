05c76327ea7edef39a83a955a7ff5eba70c457bf
Matt Keane <mattpkeane@gmail.com>
2020-07-31 11:48:00 -0500

    back end now creates a gif based on data from front end

--- resources/images.py
+++ resources/images.py
@@ -1,4 +1,18 @@
 from flask import Blueprint, request, jsonify
+from PIL import Image, ImageDraw
+
+black = (0, 0, 0)
+white =(255, 255, 255)
+
+def draw_gif(pixels):
+	image = Image.new("RGB", (200, 200), black)
+	draw = ImageDraw.Draw(image)
+	for i in range(len(pixels)):
+		for j in range(len(pixels[i])):
+			colors = [black, white]
+			color = colors[pixels[i][j]]
+			draw.rectangle([(j * 20, i * 20), (j * 20 + 20), (i * 20 + 20)], outline=color, fill=color)
+	image.save("static/client/test.gif")
 
 images = Blueprint("images", "images")
 
@@ -6,6 +20,7 @@ images = Blueprint("images", "images")
 def new_image():
 	payload = request.get_json()
 	print(payload)
+	draw_gif(payload)
 	return jsonify(
 		data={},
 		message="Check terminal",
