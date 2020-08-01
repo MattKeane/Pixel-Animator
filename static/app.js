30f0999a84c9cb420f86b125d3604234b617d63c
Matt Keane <mattpkeane@gmail.com>
2020-08-01 13:38:09 -0500

    front end now supports multiple frames

--- static/app.js
+++ static/app.js
@@ -1,70 +1,80 @@
 const board = {
 
 	// array to hold values representing pixels
-	pixels: [],
+	frames: [],
 
 	// event handlers
 
 	handleEvent: function(e) {
 		if (e.target.className === "pixel") {
 			e.target.setAttribute("class", "white-pixel")
-			this.pixels[e.target.dataset.y][e.target.dataset.x] = 1
+			this.frames[e.target.dataset.frame][e.target.dataset.y][e.target.dataset.x] = 1
 		} else if (e.target.className === "white-pixel") {
 			e.target.setAttribute("class", "pixel")
-			this.pixels[e.target.dataset.y][e.target.dataset.x] = 0
+			this.frames[e.target.dataset.frame][e.target.dataset.y][e.target.dataset.x] = 0
 		} else {
 			console.log("Error: target's class name is invalid")
 		}
 	},	
 
 	// method to create a new pixel in DOM and state
-	createNewPixel: function(x, y) {
-		this.pixels[y].push(0)
+	createNewPixel: function(x, y, frame) {
+		this.frames[frame][y].push(0)
 		const newPixel = document.createElement("div")
 		newPixel.setAttribute("class", "pixel")
 		newPixel.setAttribute("data-x", x)
 		newPixel.setAttribute("data-y", y)
+		newPixel.setAttribute("data-frame", frame)
 		return newPixel
 	},
 
 	// method to create new row in DOM and state
-	createNewRow: function(row, length) {
-		this.pixels.push([])
+	createNewRow: function(row, length, frame) {
+		this.frames[frame].push([])
 		const newRow = document.createElement("div")
 		newRow.setAttribute("class", "row")
 		for (let i = 0; i < length; i++) {
-			newRow.appendChild(this.createNewPixel(i, row))
+			newRow.appendChild(this.createNewPixel(i, row, frame))
 		}
 		return newRow
 	},
 
 	// method to initialize the board
 
-	create: function(width, height) {
-		const newBoard = document.createElement("div")
-		newBoard.setAttribute("class", "board")
-		for (let i = 0; i < height; i++) {
-			newBoard.appendChild(this.createNewRow(i, width))
+	create: function(width, height, frames) {
+		for (let i = 0; i < frames; i++) {
+			this.frames.push([])
+			const newBoard = document.createElement("div")
+			if (i === 0) {
+				newBoard.setAttribute("class", "board")
+			} else {
+				newBoard.setAttribute("class", "invisible-board")
+			}
+			
+			for (let j = 0; j < height; j++) {
+				newBoard.appendChild(this.createNewRow(j, width, i))
+			}
+			newBoard.addEventListener("mousedown", e => this.handleEvent(e))
+			document.getElementById("board-container").appendChild(newBoard)
 		}
-		newBoard.addEventListener("mousedown", e => this.handleEvent(e))
-		return newBoard
 	},
 
 	submit: async function() {
-		const submitResponse = await fetch("/images/", {
-			method: "POST",
-			body: JSON.stringify(this.pixels),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		const submitJson = await submitResponse.json()
-		if (submitJson.status === 200) {
-			window.open(`/images/${submitJson.data.image_uuid}`, "_blank")
-		}
+		console.log(this.frames)
+		// const submitResponse = await fetch("/images/", {
+		// 	method: "POST",
+		// 	body: JSON.stringify(this.pixels),
+		// 	headers: {
+		// 		"Content-Type": "application/json"
+		// 	}
+		// })
+		// const submitJson = await submitResponse.json()
+		// if (submitJson.status === 200) {
+		// 	window.open(`/images/${submitJson.data.image_uuid}`, "_blank")
+		// }
 	}
 }
 
-document.getElementById("board-container").appendChild(board.create(10, 10))
+board.create(10, 10, 10)
 
 document.getElementById("submit-button").addEventListener("click", e => board.submit())
