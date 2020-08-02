b2d06bd906f8b44cbed428a9e07f5ed7fda6cc89
Matt Keane <mattpkeane@gmail.com>
2020-08-02 12:06:15 -0500

    removed divs

--- static/app.js
+++ static/app.js
@@ -38,62 +38,22 @@ const board = {
 		}
 	},
 
-	// method to create a new pixel in DOM and state
-	createNewPixel: function(x, y, frame) {
-		this.frames[frame][y].push(0)
-		const newPixel = document.createElement("td")
-		newPixel.setAttribute("class", "pixel")
-		newPixel.setAttribute("data-x", x)
-		newPixel.setAttribute("data-y", y)
-		newPixel.setAttribute("data-frame", frame)
-		return newPixel
-	},
-
-	// method to create new row in DOM and state
-	createNewRow: function(row, length, frame) {
-		this.frames[frame].push([])
-		const newRow = document.createElement("tr")
-		newRow.setAttribute("class", "row")
-		for (let i = 0; i < length; i++) {
-			newRow.appendChild(this.createNewPixel(i, row, frame))
-		}
-		return newRow
-	},
 
 	// method to initialize the board
 
 	create: function(width, height, frames) {
 		for (let i = 0; i < frames; i++) {
 			this.frames.push([])
-			const newBoard = document.createElement("table")
-			newBoard.setAttribute("id", `frame-${i}`)
-			if (i === 0) {
-				newBoard.setAttribute("class", "board")
-			} else {
-				newBoard.setAttribute("class", "invisible-board")
-			}
-			
 			for (let j = 0; j < height; j++) {
-				newBoard.appendChild(this.createNewRow(j, width, i))
+				this.frames[i].push([])
+				for (let k = 0; k < width; k ++) {
+					this.frames[i][j].push(0)
+				}
 			}
-			newBoard.addEventListener("mousedown", e => this.handleDivClick(e))
-			document.getElementById("board-container").appendChild(newBoard)
 		}
 	},
 
 	// event handlers
-
-	handleDivClick: function(e) {
-		if (e.target.className === "pixel") {
-			e.target.setAttribute("class", "white-pixel")
-			this.frames[e.target.dataset.frame][e.target.dataset.y][e.target.dataset.x] = 1
-		} else if (e.target.className === "white-pixel") {
-			e.target.setAttribute("class", "pixel")
-			this.frames[e.target.dataset.frame][e.target.dataset.y][e.target.dataset.x] = 0
-		} else {
-			console.log("Error: target's class name is invalid")
-		}
-	},	
 
 	submit: async function() {
 		const submitResponse = await fetch("/images/", {
@@ -110,13 +70,11 @@ const board = {
 	},
 
 	handleSelectChange: function(e) {
-		document.getElementById(`frame-${this.selectedFrame}`).setAttribute("class", "invisible-board")
 		this.selectedFrame = e.target.value
-		document.getElementById(`frame-${e.target.value}`).setAttribute("class", "board")
+		this.drawFrame(this.frames[this.selectedFrame])
 	},
 
 	handleCanvasClick: function(e) {
-		console.log("Is sane")
 		const x = Math.floor(e.offsetX / 20)
 		const y = Math.floor(e.offsetY / 20)
 		this.frames[this.selectedFrame][y][x] = this.frames[this.selectedFrame][y][x] ? 0 : 1
