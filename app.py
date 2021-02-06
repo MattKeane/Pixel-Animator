6698e5eb08b4460a1d372dd132a1e30c5e5365b6
Matt Keane <mattpkeane@gmail.com>
2021-02-06 15:08:20 -0600

    updated CORS for heroku

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ PORT = 8000
 
 app = Flask(__name__)
 
+app.config.update(
+  SESSION_COOKIE_SECURE=True,
+  SESSION_COOKIE_SAMESITE='None'
+)
+
 CORS(images, origins=["http://localhost:3000", "https://pixel-wizard.herokuapp.com"], supports_credentials=True)
 
 app.register_blueprint(images, url_prefix="/images/")
